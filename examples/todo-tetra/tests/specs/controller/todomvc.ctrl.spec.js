3fb8cedae0eb2614fac86e847298ff7ff6bb5aae
Thaume <tom.coquereau@gmail.com>
2013-02-08 14:54:41 +0100

    ADD setting the frame for todomvc app tests

---
+++ examples/todo-tetra/tests/specs/controller/todomvc.ctrl.spec.js
@@ -0,0 +1,43 @@
+/* ###########################################
+#
+# Specs for TodoMVC's Tetra.js implementation
+#
+# Controller side
+#
+#############################################*/
+
+describe("the todos controller", function() {
+
+  "use strict";
+
+  // Config
+  beforeEach(function() {
+
+  });
+    
+  afterEach(function() {
+
+  });
+
+  // Tests
+  it("should create a todo", function() {
+
+  });
+  
+  it("should retrieve all todos", function() {
+      
+  });
+
+  it("should retrieve one todo", function() {
+      
+  });
+
+  it("should update one todo", function() {
+      
+  });
+
+  it("should delete one todo", function() {
+      
+  });
+
+});
